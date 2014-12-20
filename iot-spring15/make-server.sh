69a0ef7be1ae0a2655b39d1a4c2d9862b5f5a46e
Erik Wilde <dret@berkeley.edu>
2014-12-19 17:09:07 -0800

    fixing saxon path

--- iot-spring15/make-server.sh
+++ iot-spring15/make-server.sh
@@ -1,5 +1,5 @@
 git pull origin master
 ( cd hotspot ; svn update )
-~/java/bin/java -jar ../../../lectures/saxon/saxon.jar iot-spring15.xml hotspot/hotspot/hotspot.xsl messages=informative
+~/java/bin/java -jar ../saxon/saxon.jar iot-spring15.xml hotspot/hotspot/hotspot.xsl messages=informative
 cd a
-~/java/bin/java -jar ../../../../lectures/saxon/saxon.jar iot-spring15-assignments.xml assignments2html.xsl
+~/java/bin/java -jar ../../saxon/saxon.jar iot-spring15-assignments.xml assignments2html.xsl
