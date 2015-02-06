a98efff8e13a04376188c3d6c542f1200c193c00
Erik Wilde <dret@berkeley.edu>
2015-02-06 14:00:27 -0800

    moving assignments to bCourses

--- iot-spring15/make-server.sh
+++ iot-spring15/make-server.sh
@@ -1,5 +1,3 @@
 git pull origin master
 ( cd hotspot ; svn update )
 ~/java/bin/java -jar ../saxon/saxon.jar iot-spring15.xml hotspot/hotspot/hotspot.xsl messages=informative
-cd a
-~/java/bin/java -jar ../../saxon/saxon.jar iot-spring15-assignments.xml assignments2html.xsl
--- iot-spring15/make.sh
+++ iot-spring15/make.sh
@@ -1,5 +1,3 @@
 git pull origin master
 ( cd hotspot ; svn update )
 java -jar ../saxon/saxon.jar iot-spring15.xml hotspot/hotspot/hotspot.xsl messages=informative
-cd a
-java -jar ../../saxon/saxon.jar iot-spring15-assignments.xml assignments2html.xsl
