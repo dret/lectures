02de034421a906aa2d5ef8ab1bfda6cdf1be0562
Erik Wilde <dret@berkeley.edu>
2015-05-23 13:39:46 +0200

    adjusting make files

--- kesw2015/make.sh
+++ kesw2015/make.sh
@@ -1,3 +1,3 @@
 git pull origin master
 ( cd hotspot ; svn update )
-java -jar ../saxon/saxon.jar wsrest2015.xml hotspot/hotspot/hotspot.xsl messages=informative
+java -jar ../saxon/saxon.jar kesw2015.xml hotspot/hotspot/hotspot.xsl messages=informative
