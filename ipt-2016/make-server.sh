b359a14c45e15a67b8a97001719a0c23d532b3dc
Erik Wilde <erik.wilde@dret.net>
2016-08-02 16:29:46 -0700

    better filename

--- ipt-2016/make-server.sh
+++ ipt-2016/make-server.sh
@@ -1,3 +1,3 @@
 git pull origin master
 ( cd hotspot ; svn update )
-~/java/bin/java -jar ../saxon/saxon.jar apilandscaping.xml hotspot/hotspot/hotspot.xsl messages=informative
+~/java/bin/java -jar ../saxon/saxon.jar api-landscaping.xml hotspot/hotspot/hotspot.xsl messages=informative
