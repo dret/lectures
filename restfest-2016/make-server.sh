560d3fba31bf639164f9caf41c042d87daa05e33
Erik Wilde <erik.wilde@dret.net>
2016-09-15 00:39:10 -0400

    changing file and title

--- restfest-2016/make-server.sh
+++ restfest-2016/make-server.sh
@@ -1,3 +1,3 @@
 git pull origin master
 ( cd hotspot ; svn update )
-~/java/bin/java -jar ../saxon/saxon.jar dissecting-the-web.xml hotspot/hotspot/hotspot.xsl messages=informative
+~/java/bin/java -jar ../saxon/saxon.jar deconstructing-the-web.xml hotspot/hotspot/hotspot.xsl messages=informative
--- restfest-2016/make.sh
+++ restfest-2016/make.sh
@@ -1,3 +1,3 @@
 git pull origin master
 ( cd hotspot ; svn update )
-java -jar ../saxon/saxon.jar dissecting-the-web.xml hotspot/hotspot/hotspot.xsl messages=informative
+java -jar ../saxon/saxon.jar deconstructing-the-web.xml hotspot/hotspot/hotspot.xsl messages=informative
