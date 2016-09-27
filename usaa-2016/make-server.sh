178ddaefb023efecadd0b1f2e77ea1fed13b9834
Erik Wilde <erik.wilde@dret.net>
2016-09-27 20:07:36 +0200

    better naming

--- usaa-2016/make-server.sh
+++ usaa-2016/make-server.sh
@@ -1,3 +1,3 @@
 git pull origin master
 ( cd hotspot ; svn update )
-~/java/bin/java -jar ../saxon/saxon.jar api.xml hotspot/hotspot/hotspot.xsl messages=informative
+~/java/bin/java -jar ../saxon/saxon.jar usaa.xml hotspot/hotspot/hotspot.xsl messages=informative
