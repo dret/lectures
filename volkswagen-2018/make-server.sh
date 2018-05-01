04efabe7b72a33b6c3d2618a6c3c0c5545990e43
Erik Wilde <erik.wilde@dret.net>
2018-05-01 10:22:16 +0200

    reorg

--- volkswagen-2018/make-server.sh
+++ volkswagen-2018/make-server.sh
@@ -1,3 +1,4 @@
 git pull origin master
 ( cd hotspot ; svn update )
-~/java/bin/java -jar ../saxon/saxon.jar volkswagen.xml hotspot/hotspot/hotspot.xsl messages=informative
+~/java/bin/java -jar ../saxon/saxon.jar march.xml hotspot/hotspot/hotspot.xsl messages=informative
+~/java/bin/java -jar ../saxon/saxon.jar may.xml hotspot/hotspot/hotspot.xsl messages=informative
--- volkswagen-2018/make.sh
+++ volkswagen-2018/make.sh
@@ -1,3 +1,4 @@
 git pull origin master
 ( cd hotspot ; svn update )
-java -jar ../saxon/saxon.jar volkswagen.xml hotspot/hotspot/hotspot.xsl messages=informative
+java -jar ../saxon/saxon.jar march.xml hotspot/hotspot/hotspot.xsl messages=informative
+java -jar ../saxon/saxon.jar may.xml hotspot/hotspot/hotspot.xsl messages=informative
