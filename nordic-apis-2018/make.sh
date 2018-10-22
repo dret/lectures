6d118d9095b06a75f970bb9036df594f0316d87f
dret <erik.wilde@dret.net>
2018-10-22 22:24:25 +0200

    renaming XML

--- nordic-apis-2018/make.sh
+++ nordic-apis-2018/make.sh
@@ -1,3 +1,3 @@
 git pull origin master
 ( cd hotspot ; svn update )
-java -jar ../saxon/saxon.jar amsterdam.xml hotspot/hotspot/hotspot.xsl messages=informative
+java -jar ../saxon/saxon.jar nordic-apis.xml hotspot/hotspot/hotspot.xsl messages=informative
