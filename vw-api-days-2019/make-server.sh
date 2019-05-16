530bbf1c787bb4688484d5254ae710c4d0962df9
dret <erik.wilde@dret.net>
2019-05-16 15:58:57 -0700

    fixing makefiles

--- vw-api-days-2019/make-server.sh
+++ vw-api-days-2019/make-server.sh
@@ -1,3 +1,3 @@
 ( cd hotspot ; git pull origin master )
 git pull origin master
-~/java/bin/java -jar ../saxon/saxon.jar vw-api-days-2019.xml hotspot/hotspot/hotspot.xsl messages=informative
+~/java/bin/java -jar ../saxon/saxon.jar vw-api-days.xml hotspot/hotspot/hotspot.xsl messages=informative
