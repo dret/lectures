4bc2eab378e9c1a5bb048bf279d80dc596a04d14
dret <erik.wilde@dret.net>
2024-01-20 13:11:45 +0100

    spring cleaning

--- apicon-den-haag-2021/make.sh
+++ apicon-den-haag-2021/make.sh
@@ -1,3 +1,3 @@
 ( cd hotspot ; git pull origin master )
 git pull origin master
-java -jar ../saxon/saxon.jar apicon.xml hotspot/hotspot/hotspot.xsl messages=informative
+java -jar ../saxon/saxon.jar denhaag.xml hotspot/hotspot/hotspot.xsl messages=informative
